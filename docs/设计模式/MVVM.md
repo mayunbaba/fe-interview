1f12657ec24fd9b3146708879a489a9527a69cb9
liuweihua <18538302307@163.com>
2024-03-08 14:59:08 +0800

    feat: 发布订阅模式

---
+++ docs/设计模式/MVVM.md
@@ -0,0 +1,8 @@
+# MVVM
+
+![alt text](image.png)
+
+
+- View（用户界面）：，主要由HTML、CSS构建
+- ViewModel（视图数据层）: 从后端获取数据转换成View层需要的数据，由于实现了双向数据绑定，不需要再去操作DOM。
+- Model：数据模型，泛指后端的数据
--- docs/设计模式/发布订阅.js
+++ docs/设计模式/发布订阅.js
@@ -1,3 +1,4 @@
+// 事件总线
 class EventBus {
   constructor() {
     this.events = {};
@@ -23,15 +24,23 @@ class EventBus {
 const eventBus = new EventBus();
 
 // 订阅者1 （米粉）
-eventBus.$on('雷军', (product, message) => {
-  console.log(`雷军：${product} ${message} 买买买`);
-});
+eventBus.$on('雷军', buy);
 
 // 订阅者2 （米黑）
-eventBus.$on('雷军', (product, message) => {
-  console.log(`雷军：${product} ${message} 组装厂`);
-});
+eventBus.$on('雷军', notBuy);
 
 // 发布者 （雷军）
 eventBus.$emit('雷军', '小米14发布', '预售开始了');
-eventBus.$emit('雷军', '小米su7发布', '我们很贵');
+eventBus.$emit('雷军', '小米su7发布', '定价确实有点贵');
+console.log('米粉: 我们只是长大了，不是变富了, 买不起了, 取关了');
+eventBus.$off('雷军', buy);
+eventBus.$emit('雷军', '小米su7降价了', '9.9W，预售开始了');
+
+
+function buy(product) {
+  console.log(`米粉：${product}, 我决定买买买`);
+}
+
+function notBuy(product, message) {
+  console.log(`米黑：${product}, ${message}，工业垃圾，不买不买不买`);
+}
