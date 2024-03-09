ca79225f19096e3d2eaccfdb797ce1a87967ee3e
liuweihua <18538302307@163.com>
2024-03-09 12:25:56 +0800

    feat: 修改链接

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -84,7 +84,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/mayunbaba/fe-interview" },
     ],
   },
 });
