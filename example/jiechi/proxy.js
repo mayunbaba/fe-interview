23930840f94a24a888ce25e16affec11b1da3eac
liuweihua <18538302307@163.com>
2024-03-09 09:59:43 +0800

    feat: 数据代理

--- example/jiechi/proxy.js
+++ example/jiechi/proxy.js
@@ -6,14 +6,17 @@ const obj3 = {
   }
 }
 
-function proxyObj(obj) {
+function proxyObj(obj, expr = '__root__') {
+  if (typeof obj !== 'object') {
+    return obj
+  }
   return new Proxy(obj, {
     get(target, key) {
-      console.log('get', key)
-      return target[key]
+      console.log('get', expr + '.' + key, target[key])
+      return proxyObj(target[key], expr + '.' + key)
     },
     set(target, key, value) {
-      console.log('set', key, value)
+      console.log('set', expr + '.' + key, value)
       target[key] = value
     },
   })
@@ -24,6 +27,6 @@ const proxy = proxyObj(obj3)
 console.log('--proxy开始--')
 console.log(proxy.name) // get name vue3
 proxy.version = '3.0.1' // set version 3.0.1
-console.log(proxy.a) // get a { b: 1 }
+console.log(proxy.a.b) // get a { b: 1 }
 console.log('只能代理第一层属性，a.b无法代理')
 console.log('--proxy结束--')
